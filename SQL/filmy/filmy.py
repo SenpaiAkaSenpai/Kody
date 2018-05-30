84ed9a32b3ad5b602d4b97d3e652ef5295b7404c
SenpaiAkaSenpai <jakub.gagat@onet.pl>
2018-05-30 08:45:31 +0200

    fajnie

--- SQL/filmy/filmy.py
+++ SQL/filmy/filmy.py
@@ -2,15 +2,34 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import csv
+
+
+def dane_z_pliku(nazwa_pliku):
+    dane = []
+    with open(nazwa_pliku, 'r', newline='', encoding='utf-8') as plik:
+        tresc = csv.reader(plik, delimiter='\t')
+        for rekord in tresc:
+            rekord = [x.strip() for x in rekord]
+            dane.append(rekord)
+        print(dane)
 
 
 def main(args):
     con = sqlite3.connect('filmy.db')
     cur = con.cursor()
 
+    # utworzenie tabeli w bazie
     with open('filmy.sql', 'r') as plik:
         cur.executescript(plik.read())
 
+        # dodawanie bazy do danych
+        filmy = dane_z_pliku('filmy.txt')
+        filmy.pop(0)
+        cur.executemany('INSERT INTO filmy VALUES(?, ?, ?, ?, ?)', filmy)
+
+    con.commit()
+    con.close()
     return 0
 
 
