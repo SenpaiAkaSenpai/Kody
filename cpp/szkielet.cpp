afbd9c3906631be28847a6872aac746879119f67
SenpaiAkaSenpai <jakub.gagat@onet.pl>
2018-09-07 08:45:21 +0200

    powrót króla bby

---
+++ cpp/szkielet.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	
+    int liczba;
+    
+    liczba = 7312;
+    cout << liczba;
+    
+    int a, b, c;
+    
+    a = b = c = 0;
+    a = 5;
+    b = 756; 
+    c = b - 10(a);
+    cout << "\n" << a << "  " << b << "  " << (b - a);
+    cout << "\n" << d;
+    
+    
+	return 0;
+}
+
