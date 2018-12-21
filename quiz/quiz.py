1b6699f642b6c989004b895b82dd56d95a774cfa
SenpaiAkaSenpai <jakub.gagat@onet.pl>
2018-12-21 08:45:22 +0100

    informatyka na bok teraz jem sernik

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,40 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Flask
-from flask import render_template, request
-app = Flask(__name__)
 
-print(__name__)
+from flask import g
+from modele import *
+from views import *
+
+# konfiguracja aplikacji
+app.config.update(dict(
+    SECRET_KEY='kurdeblaszka',
+))
 
 @app.before_request
 def before_request():
     g.db = baza
     g.db.connect()
-    
+
 @app.after_request
-def after_request(response)
+def after_request(response):
     g.db.close()
     return response
-    
-@app.route("/quiz")
-def quiz()
-    pytanai = Pytanie.select()
-    return render_template('quiz.html', query = pytania)
-
-@app.route("/")
-def hello():
-    return "<h1>Siema eniu</h1><h2>dobri mudzin</h2>"
-    
-@app.route("/strona")
-def strona():
-    return render_template('strona.html')
-    
-@app.route("/klasa")
-def klasa():
-    return render_template('klasa.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
