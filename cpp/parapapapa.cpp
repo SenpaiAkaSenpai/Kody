c73110e964a579d5f5a977acd1e9b7c01d0d1f8b
SenpaiAkaSenpai <jakub.gagat@onet.pl>
2019-02-06 11:41:19 +0100

    luzuj gume ziomus

--- cpp/parapapapa.cpp
+++ cpp/parapapapa.cpp
@@ -5,6 +5,29 @@ using namespace std;
 
 int cyfry[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 65, 66, 67, 68, 69, 70};
 
+void any2dec(){
+    char liczba[9];;
+    int podstawa = 0;
+ 
+    do {
+        cout << "Podstawa [2;16]: ";
+        cin >> podstawa;
+    } while(podstawa < 2 || podstawa > 16);
+
+    cout << "Ile cyfr? ";
+    cin >> ile;
+    for(int i = 0; i < ile; i++)
+        do{
+            cout << "Podaj cyfrę [0-" << podstawa - 1 << "]: ";
+            cin >> tab[i];
+        }while (tab[i] < 0 || tab[i] > podstawa);
+            for (int i = 0; i < ile; i++){
+                liczba10 += tab[i] * pow(tab[i], ile-1-i);
+            }
+        cout << "Wynik: " << liczba10;
+}
+    
+
 int dec2any(int liczba, int podstawa, int tab[]) {
     int i = 0;
     do {
@@ -32,6 +55,8 @@ void bin2dec(int tab[]){
         }while (tab[i] < 0 || tab[i] > podstawa);
             for (int i = 0; i < ile; i++){
                 liczba10 += tab[i] * pow(tab[i], ile-1-i);
+            }
+        cout << "Wynik: " << liczba10;
 }
     
 int main(int argc, char **argv)
@@ -43,10 +68,16 @@ int main(int argc, char **argv)
     int i = dec2any(liczba, podstawa, tab);
     cout << "Wynik: ";
     while (i > -1) {
-        cout << tab[i];
+        if (podstawa > 10 && tab[i] > 10)
+            cout << (char)cyfry[tab[i]];
+        else
+            cout << tab[i];
         i--;
-        };
-    return 0;
+        }
+        
+        cout << endl;
+        bin2dec(tab);
+        return 0;
 }
 
     
